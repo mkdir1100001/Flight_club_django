cf98f6f777ad70c4263c60263836e9e2087ad810
mkdir1100001 <mkdir.1100001@gmail.com>
2023-02-14 12:55:27 +0000

    settings update

--- flight_club/settings/production.py
+++ flight_club/settings/production.py
@@ -143,4 +143,4 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+# STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
